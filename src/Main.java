e16034185fe8909db681386f7ac0b144e85582dc
2-Eung <eung4256@gmail.com>
2025-06-19 13:16:31 +0900

    Generics ArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Box {
-    public static <T> void displayArray(T[] array) {
-        for (T element : array) {
+    public static <T> void displayArray(List<T> array) {
+        for(T element: array) {
             System.out.println(element);
         }
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Integer[] numbers = {1, 2, 3, 4, 5};
-        String[] words = {"aaa", "bbb", "ccc"};
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        List<String> words = new ArrayList<>();
+        words.add("apple");
+        words.add("banana");
+        words.add("cherry");
 
         Box.displayArray(numbers);
         Box.displayArray(words);
-
     }
 }
