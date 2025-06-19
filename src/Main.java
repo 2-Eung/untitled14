31c75c203b5d33251a4b31983913445a84556c21
2-Eung <eung4256@gmail.com>
2025-06-19 13:46:14 +0900

    Generics ArrayList without .add()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 class Box {
     public static <T> void displayArray(List<T> array) {
@@ -8,15 +9,16 @@ class Box {
         }
     }
 }
+
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
+//        List<Integer> numbers = List.of(1, 2, 3, 4, 5); // 이 경우는 ArrayList 가 아니다.
+//        numbers.add(1);                                 // 이 경우 불변한녀석이어서 불가능
 
+        List<String> numbers = new ArrayList<>(List.of("1", "2", "3", "4", "5"));
+                                                          // 옳게 ArrayList 가 된녀석.
+        List<String> numberss = new ArrayList<>(Arrays.asList("1", "2", "3"));
+                                                          // 또는 이렇게
         List<String> words = new ArrayList<>();
         words.add("apple");
         words.add("banana");
