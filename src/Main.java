dc44360f713764bffcbd48a2cf27ac038c73d51c
2-Eung <eung4256@gmail.com>
2025-06-19 09:27:52 +0900

    Map HashMap .containsKey .containsValue

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,10 @@ public class Main {
         fruitPrices.put("배", 10320);
         fruitPrices.put("수박", 15920);
         fruitPrices.put("멜론", 10630);
-        fruitPrices.put("멜론", 50000);          // 키값이 중복되면 나중에 put 된것으로 덮어쓴다
 
-        System.out.println(fruitPrices.size()); // 그냥 4가 나옴
-        // 전체를 순회하려면
-        for (Map.Entry<String, Integer> entry : fruitPrices.entrySet()) {   // .entrySet() 필요
-            System.out.print(entry.getKey() + " 가격은 ");          // 키값 출력
-            System.out.print(entry.getValue() + " 원 입니다.\n");   // 벨류값 출력
-        }
-        // 키값과 벨류값을 둘 다 꺼내오고 싶으면 'Entry' (Map.Entry<,> entry new : fruitPrices.entrySet())
-        int sum = 0;
-        for (Integer fruitPrice : fruitPrices.values()) {
-            sum += fruitPrice;
-        }
-        // 둘중 하나만 사용하고 싶으면 사용하려는 타입을가진 요소로 foreach.
-        // 여기서는 Integer fruitPrice : ( int 로 대체 가능 )
-
-        System.out.println(sum/fruitPrices.size());
+        System.out.println(fruitPrices.containsKey("사과"));  // contain 은 true/false 로 반환한다.
+        System.out.println(fruitPrices.containsKey("포도"));
+        System.out.println(fruitPrices.containsValue(17980));
+        System.out.println(fruitPrices.containsValue(9999));
     }
 }
