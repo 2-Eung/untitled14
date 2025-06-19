ef34716dec94cc77b9e6e23593c3ee5295bc30df
2-Eung <eung4256@gmail.com>
2025-06-19 09:45:52 +0900

    Map HashMap .replace(,) .remove() .keySet()

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,18 @@ public class Main {
         fruitPrices.put("배", 10320);
         fruitPrices.put("수박", 15920);
         fruitPrices.put("멜론", 10630);
+//        fruitPrices.put("사과", 8000);         // 이 경우에도 순서는 맨 밑으로 이동한다.
 
-        System.out.println(fruitPrices.getOrDefault("사과", 500));
-        System.out.println(fruitPrices.getOrDefault("포도", 500));
+        // put 과 replace 의 차이는 새로운값으로 덮느냐, 기존값을 변경하느냐 이다.
+        fruitPrices.replace("사과", 8000);       // 이때 순서는 맨 밑으로 이동한다.
+        fruitPrices.replace("포도", 8000);       // replace 는 기존값이 없으면 동작하지 않는다. put 은 추가
+        fruitPrices.remove("멜론");
+
+        for(Map.Entry<String, Integer> entry : fruitPrices.entrySet()) {
+            System.out.println(entry.getKey() + entry.getValue());
+        }
+        for(String key: fruitPrices.keySet()) { // Key 값만 출력
+            System.out.println(key);
+        }
     }
 }
