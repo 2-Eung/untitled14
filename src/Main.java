e7a2950fe8b48d5e93d62e86da2992c4884ec93a
2-Eung <eung4256@gmail.com>
2025-06-19 11:40:15 +0900

    Generics

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
-import java.util.Map;
-import java.util.TreeMap;
-
+class Box<T> {      // T : type     // T 는 참조형 만 가능하다
+    private T content;              // 컴파일시점에 타입을 주는데 object 클래스를 상속받은 참조타입들만
+                                    // 컴파일시점에서 타입을 줄 수 있다. 일반타입은 상속못받아 불가능
+    public Box(T content) {         // 생성자
+        this.content = content;
+    }
+    public T getContent () {
+        return content;
+    }
+    public void setContent (T content) {
+        this.content = content;
+    }
+    public <E> void printContent(E extraInfo) {     // E : extra
+        System.out.println("Content: " + content + ", Extra: " + extraInfo);
+    }
+}
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> fruitPrices = new TreeMap<>();
+        Box<String> stringBox = new Box<>("ㅇㅇㅇ");
+        System.out.println(stringBox.getContent());     // 게터
+        stringBox.setContent("ㄴㄴㄴ");                  // 세터
+        System.out.println(stringBox.getContent());     // 게터
 
-        fruitPrices.put("사과", 17980);          // Map은 put 으로 값을 넣는다. (add 가 아님)
-        fruitPrices.put("배", 10320);
-        fruitPrices.put("수박", 15920);
-        fruitPrices.put("멜론", 10630);
-
-        for(Map.Entry<String, Integer> entry: fruitPrices.entrySet()) { // 키값 기준 오름차순으로 정렬
-            System.out.println(entry.getKey() + entry.getValue());
-        }
+        stringBox.printContent(123123);         // 메서드실행
+        stringBox.printContent("ㅂㅂㅂ");        // 메서드실행
     }
 }
