d4951cf63c2dcbda1e6f566f60e23e7075adfbb4
2-Eung <eung4256@gmail.com>
2025-06-19 15:29:21 +0900

    Generics / wildCard 2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,40 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 public class Main {
-    public static void main(String[] args) {
-        Object[] arr = new Integer[3]; // 배열은 공변이다. 그래서 상위타입으로 바뀐다.
-//        arr[1] = "Hello";   // 상위타입으로 바뀌엇기때문에 문자가 입력은된다.
-                            // 그러나 실행하면 런타임 오류가 발생한다. 불안정해진다.
-//        List<Object> arr = new ArrayList<Integer>();            // 제네릭은 불공변 이다. 컴파일오류발생
 
-        List<? extends Number> arra = new ArrayList<Integer>(); // 제네릭에서 공변성을 부여하기위한 작업.
+    public static void printElements(List<?> list) {
+        for (Object o : list) {
+            System.out.println(o);
+        }
+    }
+    public static double sumList(List<? extends Number> list) {
+        double sum = 0;
+        for (Number n : list) {             // n 의 요소는 Number 이다.
+            sum += n.doubleValue();     // .doubleValue() 는 뭐지???
+                                        // List 의 값을 꺼내오는거랑관련있나??
+                                        // get 함수 같은건가?
+            if(n instanceof Integer) {          // Number 중 Integer 라면?
+                Integer.toHexString((Integer) n);
+            } else if (n instanceof  Double) {  // Number 중 Double 이라면?
+
+            }   // T 말고 ? extend 를 쓰는이유는 T 가 더 넓어서 예외사항이 (if) 많으면
+        }       // ? extend 는 한번 걸러서 예외사항을 큰범위로 한번 없애서인가??
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        List<String> words = Arrays.asList("a", "b", "c");
+
+        printElements(numbers);
+        printElements(words);
+        System.out.println(sumList(numbers));
+
+        List<Integer> intList = Arrays.asList(10, 20, 30);
+        List<Double> doubleList = Arrays.asList(0.1, 0.2);
+
+        System.out.println("합계: " + sumList(intList));
+        System.out.println("합계: " + sumList(doubleList));
+
     }
 }
